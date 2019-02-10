171d541300cecb4b9043fd5a976918a5d40b3d76
Louie O'Grady <l.ogrady@gmail.com>
2019-02-10 10:09:27 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -16,7 +16,5 @@ def first_challenge
 
 contacts["Freddy Mercury"][:favorite_icecream_flavors].delete_if {|x| x == "strawberry"}
 contacts
-  #remember to return your newly altered contacts hash!
-  contacts
 end
 
